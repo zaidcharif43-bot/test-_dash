27d88b61806ad10900882508d2f51d2169054991
Mostafa Oulahyan <mostaphaoulahyan@gmail.com>
2026-05-31 21:39:46 +0100

    Delete scratch_password.js

--- scratch_password.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const lines = fs.readFileSync('test-_dash/src/app/page.tsx', 'utf8').split('\n');
-lines.forEach((line, idx) => {
-  if (line.includes('teamPasswordDrafts')) {
-    console.log(`${idx+1}: ${line.trim()}`);
-  }
-});
